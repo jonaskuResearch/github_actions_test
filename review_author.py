40b3ec2bd12e74ef34c16bc51deae2493afa4ef5
Jonas Kuckling <jonas.kuckling@ulb.be>
2023-01-11 19:08:42 +0100

    Trying better error handling

--- review_author.py
+++ review_author.py
@@ -3,7 +3,7 @@ import sys
 def check_alphabetical_ordering(author_file):
     last_author_key = None
     ordering_error_found = False
-    for line in author_file:
+    for line_no, line in enumerate(author_file):
         if not line.strip().startswith("%") and not line.strip().startswith("@comment") and line.strip():  # ignore comments and empty lines
             line = line.strip()
             # print(line)
@@ -11,7 +11,8 @@ def check_alphabetical_ordering(author_file):
             author_key = line.split("=")[0].split("{")[1].strip()
             # print(author_key)
             if last_author_key and last_author_key.lower() > author_key.lower():
-                print(f"Ordering error between {last_author_key} and {author_key}")
+                # print(f"Ordering error between {last_author_key} and {author_key}")
+                print(f"::error file=author.bib,line={line_no}::Ordering error between {last_author_key} and {author_key}")
                 ordering_error_found = True
             last_author_key = author_key
     return ordering_error_found
@@ -25,6 +26,6 @@ def review_author_file():
 if __name__ == "__main__":
     found_error = review_author_file()
     if found_error:
-        # sys.exit(1)
-        print("::error file=author.bib::message=There was an error") 
+        sys.exit(1)
+        # print("::error file=author.bib::message=There was an error") 
 
